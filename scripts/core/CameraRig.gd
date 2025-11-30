88166adbb0de945b55b33e6506b97aec28e1a6e3
star-tide <bnesslage223@yahoo.com>
2025-11-29 22:32:49 -0500

    Aim camera rig at focus point and keep focus during pan/zoom

--- scripts/core/CameraRig.gd
+++ scripts/core/CameraRig.gd
@@ -5,6 +5,7 @@ class_name CameraRig
 @export var zoom_step: float = 2.0
 @export var min_zoom_distance: float = 6.0
 @export var max_zoom_distance: float = 40.0
+@export var focus_point: Vector3 = Vector3(12, 0, 12)
 
 var camera: Camera3D
 var camera_direction: Vector3 = Vector3.ZERO
@@ -14,8 +15,10 @@ var last_mouse_position: Vector2 = Vector2.ZERO
 
 func _ready() -> void:
 	camera = $Camera3D
-	camera_direction = camera.transform.origin.normalized()
-	target_distance = camera.transform.origin.length()
+	var initial_offset: Vector3 = camera.transform.origin - focus_point
+	camera_direction = initial_offset.normalized()
+	target_distance = initial_offset.length()
+	camera.look_at(focus_point, Vector3.UP)
 	_apply_zoom()
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -38,8 +41,9 @@ func _adjust_zoom(delta_dist: float) -> void:
 	_apply_zoom()
 
 func _apply_zoom() -> void:
-	var new_position: Vector3 = camera_direction * target_distance
+	var new_position: Vector3 = focus_point + camera_direction * target_distance
 	camera.transform.origin = new_position
+	camera.look_at(focus_point, Vector3.UP)
 
 func _pan_camera(relative: Vector2) -> void:
 	var camera_basis: Basis = camera.global_transform.basis
@@ -49,3 +53,5 @@ func _pan_camera(relative: Vector2) -> void:
 	var delta_z: float = -relative.y * drag_pan_speed
 	var pan_vector: Vector3 = (right * delta_x) + (forward * delta_z)
 	global_position += pan_vector
+	focus_point += pan_vector
+	_apply_zoom()
